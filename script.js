da90d6e176ee0f98b6bd4d4a55a312715f3313f8
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-16 01:04:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function calculateGrade(percentage) {
-  //your code here
+	let x=percentage;
+	if(x>=90){
+		alert(`A`);
+	}
+	else if(x>=80 && x<=89){
+		alert(`B`);
+	}
+	else if(x>=70 && x<=79){
+		alert(`C`);
+	}
+	else if(x>=60 && x<=69){
+		alert(`D`);
+	}
+	else{
+		alert(`F`);
+	}
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
